072d2545c3eb2592fa4e95181367e5513e939c83
michsko544 <michsko544@student.polsl.pl>
2019-09-30 14:35:26 +0200

    Add basic hero image view

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,16 @@
   </div>
 </template>
 
-<style lang="scss" scoped/>
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,600,800&display=swap');
+
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Montserrat', sans-serif;
+    margin:0;
+    padding:0;
+  }
+</style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,14 +1,7 @@
 <template>
   <div class="wrapper">
-    <div class="search">
-      <label for="search">Search</label>
-      <input name="search" id="search" v-model="searchValue" @input="handleInput">
-      <ul>
-        <li v-for="item in results" :key="item.data[0].nasa_id">
-          <p>{{item.data[0].description}}</p>
-        </li>
-      </ul>
-    </div>
+    <Claim/>
+    <SearchInput/>
   </div>
 </template>
 
@@ -16,11 +9,17 @@
 // eslint-disable-next-line
 import axios from 'axios';
 import debounce from 'lodash.debounce';
+import Claim from '@/components/Claim.vue';
+import SearchInput from '@/components/SearchInput.vue';
 
 const API = 'https://images-api.nasa.gov';
 
 export default {
   name: 'Search',
+  components: {
+    Claim,
+    SearchInput,
+  },
   data() {
     return {
       searchValue: '',
@@ -47,24 +46,16 @@ export default {
     display:flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
     margin:0;
+    height: 100vh;
     padding:30px;
     width:100%;
+    background-image: url('../assets/heroimage.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 80% 0;
   }
 
-  .search{
-    display:flex;
-    flex-direction: column;
-    width:300px;
-  }
-
-  label{
-    font-family: Montserrat, sans-serif;
-  }
-
-  input{
-    height:30px;
-    border:0;
-    border-bottom: 1px solid black;
-  }
+  
 </style>
